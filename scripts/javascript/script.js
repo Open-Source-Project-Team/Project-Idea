f9a563848d8363c6f18b5482a7006a2e32aec2f4
Abir Sheikh <abir.sheikh.4044@gmail.com>
2021-07-05 11:49:00 +0600

    Added Navbar

---
+++ scripts/javascript/script.js
@@ -0,0 +1,18 @@
+// Makes navbar dynamic
+function nav(){
+    // select nav button container ( The <li> tag)
+    var nav_btn_li = document.querySelectorAll(".nav-btn-container");
+    // adds event listener to all nav buttons
+    for(let index = 0; index < nav_btn_li.length; index++){
+        nav_btn_li[index].addEventListener("click", (event_log) => {navigate(index);});
+    }
+    function navigate(class_index){
+        // reset all button to default color (makes them inactive);
+        for(let i = 0; i < nav_btn_li.length; i++){
+            nav_btn_li[i].classList.remove("active");
+        }
+        // activates the clicked nav btn
+        nav_btn_li[class_index].classList.add("active");
+    }
+}
+nav();
